bb3fda24d97576e7d0d92d88125e8e0f470fdd1f
Fredrik Aurdal <fredrik.a@protonmail.ch>
2024-01-15 12:49:44 +0100

    Updated

--- app/ui/table.js
+++ app/ui/table.js
@@ -1,9 +1,11 @@
 import { data } from '../data';
+import stars from '../../public/stars.svg';
+import Image from 'next/image';
 
 export default function Table() {
   return (
     <>
-      <section className="container px-4 mx-auto">
+      <section className="container pb-4 px-4 mx-auto">
         <div className="flex flex-col mt-6">
           <div className="overflow-x-auto">
             <div className="inline-block min-w-full align-middle">
@@ -117,14 +119,7 @@ export default function Table() {
                             </div>
                           </td>
                           <td className="px-4 py-4 text-sm whitespace-nowrap">
-                            <div>
-                              <h4 className="text-gray-700 dark:text-gray-200">
-                                Content curating app
-                              </h4>
-                              <p className="text-gray-500 dark:text-gray-400">
-                                Brings all your news into one place
-                              </p>
-                            </div>
+                            <Image src={stars} height={16} alt="Vurdering" />
                           </td>
                           <td className="px-12 py-4 text-sm font-medium whitespace-nowrap">
                             <div className="inline px-3 py-1 text-sm font-normal rounded-full text-emerald-500 gap-x-2 bg-emerald-100/60 dark:bg-gray-800">
@@ -155,92 +150,6 @@ export default function Table() {
               </div>
             </div>
           </div>
-        </div>
-        <div className="flex items-center justify-between mt-6">
-          <a
-            href="#"
-            className="flex items-center px-5 py-2 text-sm text-gray-700 capitalize transition-colors duration-200 bg-white border rounded-md gap-x-2 hover:bg-gray-100 dark:bg-gray-900 dark:text-gray-200 dark:border-gray-700 dark:hover:bg-gray-800"
-          >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              strokeWidth="1.5"
-              stroke="currentColor"
-              className="w-5 h-5 rtl:-scale-x-100"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M6.75 15.75L3 12m0 0l3.75-3.75M3 12h18"
-              />
-            </svg>
-            <span>previous</span>
-          </a>
-          <div className="items-center hidden md:flex gap-x-3">
-            <a
-              href="#"
-              className="px-2 py-1 text-sm text-blue-500 rounded-md dark:bg-gray-800 bg-blue-100/60"
-            >
-              1
-            </a>
-            <a
-              href="#"
-              className="px-2 py-1 text-sm text-gray-500 rounded-md dark:hover:bg-gray-800 dark:text-gray-300 hover:bg-gray-100"
-            >
-              2
-            </a>
-            <a
-              href="#"
-              className="px-2 py-1 text-sm text-gray-500 rounded-md dark:hover:bg-gray-800 dark:text-gray-300 hover:bg-gray-100"
-            >
-              3
-            </a>
-            <a
-              href="#"
-              className="px-2 py-1 text-sm text-gray-500 rounded-md dark:hover:bg-gray-800 dark:text-gray-300 hover:bg-gray-100"
-            >
-              ...
-            </a>
-            <a
-              href="#"
-              className="px-2 py-1 text-sm text-gray-500 rounded-md dark:hover:bg-gray-800 dark:text-gray-300 hover:bg-gray-100"
-            >
-              12
-            </a>
-            <a
-              href="#"
-              className="px-2 py-1 text-sm text-gray-500 rounded-md dark:hover:bg-gray-800 dark:text-gray-300 hover:bg-gray-100"
-            >
-              13
-            </a>
-            <a
-              href="#"
-              className="px-2 py-1 text-sm text-gray-500 rounded-md dark:hover:bg-gray-800 dark:text-gray-300 hover:bg-gray-100"
-            >
-              14
-            </a>
-          </div>
-          <a
-            href="#"
-            className="flex items-center px-5 py-2 text-sm text-gray-700 capitalize transition-colors duration-200 bg-white border rounded-md gap-x-2 hover:bg-gray-100 dark:bg-gray-900 dark:text-gray-200 dark:border-gray-700 dark:hover:bg-gray-800"
-          >
-            <span>Next</span>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              strokeWidth="1.5"
-              stroke="currentColor"
-              className="w-5 h-5 rtl:-scale-x-100"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M17.25 8.25L21 12m0 0l-3.75 3.75M21 12H3"
-              />
-            </svg>
-          </a>
         </div>
       </section>
     </>
