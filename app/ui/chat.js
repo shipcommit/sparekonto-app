2e977241a82921cd9217d2f898e9be44f4681f41
Fredrik Aurdal <fredrik.a@protonmail.ch>
2024-01-16 11:11:39 +0100

    Update chat.js

--- app/ui/chat.js
+++ app/ui/chat.js
@@ -6,33 +6,127 @@ export default function Chat() {
   const { messages, input, handleInputChange, handleSubmit } = useChat();
 
   return (
-    <div className="flex flex-col w-full max-w-md py-24 mx-auto stretch">
-      {messages.length > 0
-        ? messages.map((m) => (
-            <div key={m.id} className="whitespace-pre-wrap">
-              {m.role === 'user' ? 'User: ' : 'AI: '}
-              {m.content}
-            </div>
-          ))
-        : null}
-
-      <form
-        onSubmit={(e) => {
-          handleSubmit(e, {
-            data: {
-              imageUrl:
-                'https://upload.wikimedia.org/wikipedia/commons/thumb/3/3c/Field_sparrow_in_CP_%2841484%29_%28cropped%29.jpg/733px-Field_sparrow_in_CP_%2841484%29_%28cropped%29.jpg',
-            },
-          });
-        }}
+    <div className="container mx-auto w-full bg-white">
+      <button
+        className="fixed bottom-4 right-4 inline-flex items-center justify-center text-sm font-medium disabled:pointer-events-none disabled:opacity-50 border rounded-full w-16 h-16 bg-black hover:bg-gray-700 m-0 cursor-pointer border-gray-200 bg-none p-0 normal-case leading-5 hover:text-gray-900"
+        type="button"
+        aria-haspopup="dialog"
+        aria-expanded="false"
+        data-state="closed"
       >
-        <input
-          className="fixed bottom-0 w-full max-w-md p-2 mb-8 border border-gray-300 rounded shadow-xl"
-          value={input}
-          placeholder="What does the image show..."
-          onChange={handleInputChange}
-        />
-      </form>
+        <svg
+          xmlns=" http://www.w3.org/2000/svg"
+          width="30"
+          height="40"
+          viewBox="0 0 24 24"
+          fill="none"
+          stroke="currentColor"
+          strokeWidth="2"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          className="text-white block border-gray-200 align-middle"
+        >
+          <path
+            d="m3 21 1.9-5.7a8.5 8.5 0 1 1 3.8 3.8z"
+            className="border-gray-200"
+          ></path>
+        </svg>
+      </button>
+
+      <div className="fixed bottom-[calc(4rem+1.5rem)] right-0 mr-4 bg-white p-6 rounded-lg border border-[#e5e7eb] w-[440px] h-[634px] z-[100]">
+        <div className="flex flex-col space-y-1.5 pb-6">
+          <h2 className="font-semibold text-lg tracking-tight">Chatbot</h2>
+          <p className="text-sm text-[#6b7280] leading-3">
+            Powered by Mendable and Vercel
+          </p>
+        </div>
+
+        <div className="pr-4 h-[474px]">
+          <div className="flex gap-3 my-4 text-gray-600 text-sm flex-1">
+            <span className="relative flex shrink-0 overflow-hidden rounded-full w-8 h-8">
+              <div className="rounded-full bg-gray-100 border p-1">
+                <svg
+                  stroke="none"
+                  fill="black"
+                  strokeWidth="1.5"
+                  viewBox="0 0 24 24"
+                  ariahidden="true"
+                  height="20"
+                  width="20"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M9.813 15.904L9 18.75l-.813-2.846a4.5 4.5 0 00-3.09-3.09L2.25 12l2.846-.813a4.5 4.5 0 003.09-3.09L9 5.25l.813 2.846a4.5 4.5 0 003.09 3.09L15.75 12l-2.846.813a4.5 4.5 0 00-3.09 3.09zM18.259 8.715L18 9.75l-.259-1.035a3.375 3.375 0 00-2.455-2.456L14.25 6l1.036-.259a3.375 3.375 0 002.455-2.456L18 2.25l.259 1.035a3.375 3.375 0 002.456 2.456L21.75 6l-1.035.259a3.375 3.375 0 00-2.456 2.456zM16.894 20.567L16.5 21.75l-.394-1.183a2.25 2.25 0 00-1.423-1.423L13.5 18.75l1.183-.394a2.25 2.25 0 001.423-1.423l.394-1.183.394 1.183a2.25 2.25 0 001.423 1.423l1.183.394-1.183.394a2.25 2.25 0 00-1.423 1.423z"
+                  ></path>
+                </svg>
+              </div>
+            </span>
+            <p className="leading-relaxed">
+              <span className="block font-bold text-gray-700">AI </span>How can
+              I help you today?
+            </p>
+          </div>
+
+          {messages.length > 0
+            ? messages.map((m) => (
+                <div
+                  key={m.id}
+                  className="flex gap-3 my-4 text-gray-600 text-sm flex-1"
+                >
+                  <span className="relative flex shrink-0 overflow-hidden rounded-full w-8 h-8">
+                    <div className="rounded-full bg-gray-100 border p-1">
+                      <svg
+                        stroke="none"
+                        fill="black"
+                        strokeWidth="1.5"
+                        viewBox="0 0 24 24"
+                        ariahidden="true"
+                        height="20"
+                        width="20"
+                        xmlns="http://www.w3.org/2000/svg"
+                      >
+                        <path
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                          d="M9.813 15.904L9 18.75l-.813-2.846a4.5 4.5 0 00-3.09-3.09L2.25 12l2.846-.813a4.5 4.5 0 003.09-3.09L9 5.25l.813 2.846a4.5 4.5 0 003.09 3.09L15.75 12l-2.846.813a4.5 4.5 0 00-3.09 3.09zM18.259 8.715L18 9.75l-.259-1.035a3.375 3.375 0 00-2.455-2.456L14.25 6l1.036-.259a3.375 3.375 0 002.455-2.456L18 2.25l.259 1.035a3.375 3.375 0 002.456 2.456L21.75 6l-1.035.259a3.375 3.375 0 00-2.456 2.456zM16.894 20.567L16.5 21.75l-.394-1.183a2.25 2.25 0 00-1.423-1.423L13.5 18.75l1.183-.394a2.25 2.25 0 001.423-1.423l.394-1.183.394 1.183a2.25 2.25 0 001.423 1.423l1.183.394-1.183.394a2.25 2.25 0 00-1.423 1.423z"
+                        ></path>
+                      </svg>
+                    </div>
+                  </span>
+                  <p className="leading-relaxed">
+                    <span className="block font-bold text-gray-700">AI </span>
+                    {m.role === 'user' ? 'User: ' : 'AI: '}
+                    {m.content}
+                    {/* </div> */}
+                  </p>
+                </div>
+              ))
+            : null}
+        </div>
+
+        <div className="flex items-center pt-0">
+          <form
+            onSubmit={(e) => {
+              handleSubmit(e, {
+                data: {
+                  imageUrl:
+                    'https://upload.wikimedia.org/wikipedia/commons/thumb/3/3c/Field_sparrow_in_CP_%2841484%29_%28cropped%29.jpg/733px-Field_sparrow_in_CP_%2841484%29_%28cropped%29.jpg',
+                },
+              });
+            }}
+          >
+            <input
+              value={input}
+              placeholder="What does the image show..."
+              onChange={handleInputChange}
+            />
+          </form>
+        </div>
+      </div>
+
+      <div className="flex flex-col w-full max-w-md py-24 mx-auto stretch"></div>
     </div>
   );
 }
