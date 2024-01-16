7bc80680d1424430e85f93a6910619eb668c3a2e
Fredrik Aurdal <fredrik.a@protonmail.ch>
2024-01-16 18:00:44 +0100

    Update actions.js

--- app/lib/actions.js
+++ app/lib/actions.js
@@ -10,9 +10,7 @@ const cohere = new CohereClient({
 
 export async function createSearchEmbeddings(query) {
   try {
-    // // Connect to database
-
-    //   Check if database contains any answers to the query
+    // Get query vector embeddings
     const documentRes = await cohere.embed({
       texts: [query],
       model: 'embed-multilingual-v3.0',
@@ -23,23 +21,69 @@ export async function createSearchEmbeddings(query) {
 
     console.log('embedding:', embedding);
 
-    // // Find documents
-    // const documents = await Vector.aggregate([
-    //   {
-    //     $vectorSearch: {
-    //       queryVector: embedding,
-    //       path: 'embedding',
-    //       numCandidates: 500,
-    //       limit: limit,
-    //       index: 'vector_index',
-    //     },
-    //   },
-    // ]);
-
-    // console.log('documents:', documents);
-
-    // return documents;
+    return embedding;
   } catch (err) {
     console.log(err);
   }
 }
+
+// const body = JSON.stringify({
+//     collection: 'vectors',
+//     database: 'test',
+//     dataSource: 'Cluster0',
+//     pipeline: [
+//       {
+//         $search: {
+//           index: 'vector_index',
+
+//           compound: {
+//             should: [
+//               {
+//                 vector: {
+//                   path: 'embedding',
+//                   query: embedding,
+//                   score: { boost: { value: 1 } },
+//                 },
+//               },
+//             ],
+//           },
+//         },
+//       },
+//       { $limit: 3 },
+//     ],
+//     //   projection: {
+//     //     _id: 1,
+//     //   },
+//   });
+
+//   const databaseResponse = await fetch(
+//     'https://eu-central-1.aws.data.mongodb-api.com/app/data-eyhyj/endpoint/data/v1/action/findOne',
+//     {
+//       method: 'POST',
+//       headers: {
+//         'Content-Type': 'application/json',
+//         'Access-Control-Request-Headers': '*',
+//         'api-key': process.env.MONGODB_DATA_API_KEY,
+//       },
+//       body,
+//     }
+//   );
+
+//   console.log('databaseResponse:', databaseResponse);
+
+//   // // Find documents
+//   // const documents = await Vector.aggregate([
+//   //   {
+//   //     $vectorSearch: {
+//   //       queryVector: embedding,
+//   //       path: 'embedding',
+//   //       numCandidates: 500,
+//   //       limit: limit,
+//   //       index: 'vector_index',
+//   //     },
+//   //   },
+//   // ]);
+
+//   // console.log('documents:', documents);
+
+//   // return documents;
